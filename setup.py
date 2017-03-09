d25c7dbe3e6eb760f9d9aea4908ae9b2b67698df
Steve Brettschneider <steve@bluehousefamily.com>
2017-03-08 21:07:03 -0500

    Prep for pypi.

---
+++ setup.py
@@ -0,0 +1,16 @@
+#! /usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name = 'lazy_streams',
+    py_modules = ['lazy_streams'],
+    version = '0.1',
+    description = 'Inspired by Java 8 streams, simple list processing',
+    author = 'Steve Brettschneider',
+    author_email = 'steve@bluehousefamily.com',
+    url = 'https://github.com/brettschneider/python_lazy_streams',
+    download_url = 'https://github.com/brettschneider/python_lazy_streams/archive/0.1.tar.gz',
+    keywords = ['lazy', 'stream', 'list', 'processing', 'java'],
+    classifiers = [],
+)
