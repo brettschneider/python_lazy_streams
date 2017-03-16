2692284c894408335dbba4a072d6b497768341e9
Steve Brettschneider <steve.brettschneider@kroger.com>
2017-03-16 09:07:41 -0400

    The pypi learning curve it steep for me.

--- setup.py
+++ setup.py
@@ -5,12 +5,12 @@ from setuptools import setup
 setup(
     name = 'lazy_streams',
     py_modules = ['lazy_streams'],
-    version = '0.2',
+    version = '0.4',
     description = 'Inspired by Java 8 streams, simple list processing',
     author = 'Steve Brettschneider',
     author_email = 'steve@bluehousefamily.com',
     url = 'https://github.com/brettschneider/python_lazy_streams',
-    download_url = 'https://github.com/brettschneider/python_lazy_streams/archive/0.2.tar.gz',
+    download_url = 'https://github.com/brettschneider/python_lazy_streams/archive/0.4.tar.gz',
     keywords = ['lazy', 'stream', 'list', 'processing', 'java'],
     classifiers = [],
     install_requires=['promise-keeper']
